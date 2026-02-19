978c97219d439ba5889636c388cb17e482f4b05c
O-Kornilova <aleksandra.kopachovets.ne@gmail.com>
2026-02-19 19:22:40 +0200

    fix: links

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,15 +1,16 @@
-import { useSelector } from "react-redux";
-import { useState } from "react";
-import { Link } from "react-router-dom";
-import { Menu, X, Search } from "lucide-react";
-import avatar from "../../assets/images/ava.webp";
-import cartIcon from "../../assets/images/shopping-cart-one-pink.png";
-import styles from "./Header.module.css";
+import { useSelector } from 'react-redux'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { Menu, X, Search } from 'lucide-react'
+import avatar from '../../assets/images/ava.webp'
+import cartIcon from '../../assets/images/shopping-cart-one-pink.png'
+import styles from './Header.module.css'
 
-const Header = ({ user }) => {
-  const [menuOpen, setMenuOpen] = useState(false);
-  const { products } = useSelector((state) => state.cart);
-  const cartCount = products.reduce((sum, p) => sum + p.count, 0);
+const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false)
+  const { products } = useSelector(state => state.cart)
+  const cartCount = products.reduce((sum, p) => sum + p.count, 0)
+  const user = JSON.parse(localStorage.getItem('user'))
 
   return (
     <header className={styles.header}>
@@ -22,13 +23,13 @@ const Header = ({ user }) => {
         </button>
         {menuOpen && (
           <nav className={styles.nav}>
-            <a href="#benefits" className={styles.navLink}>
+            <a href='#benefits' className={styles.navLink}>
               Benefits
             </a>
-            <a href="#catalog" className={styles.navLink}>
+            <a href='#catalog' className={styles.navLink}>
               Catalog
             </a>
-            <a href="#answers" className={styles.navLink}>
+            <a href='#answers' className={styles.navLink}>
               Answers
             </a>
           </nav>
@@ -36,10 +37,10 @@ const Header = ({ user }) => {
       </div>
 
       {user ? (
-        <Link to="/profile">
+        <Link to='/profile'>
           <img
             src={user.avatar || avatar}
-            alt={`${user.name || "User"}'s avatar`}
+            alt={`${user.name || 'User'}'s avatar`}
             className={styles.avatar}
           />
         </Link>
@@ -49,15 +50,15 @@ const Header = ({ user }) => {
 
       <div className={styles.rightIcons}>
         <Search size={24} className={styles.searchIcon} />
-        <Link to="/cart" className={styles.cartWrapper}>
-          <img src={cartIcon} alt="Cart" className={styles.cartIcon} />
+        <Link to='/cart' className={styles.cartWrapper}>
+          <img src={cartIcon} alt='Cart' className={styles.cartIcon} />
           {cartCount > 0 && (
             <span className={styles.cartBadge}>{cartCount}</span>
           )}
         </Link>
       </div>
     </header>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
