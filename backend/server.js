73319ba547621b95df4699870eceb8d8108ccd2f
O-Kornilova <aleksandra.kopachovets.ne@gmail.com>
2026-02-27 11:07:32 +0200

    back

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,23 @@
-import express from "express";
-import dotenv from "dotenv";
-import cors from "cors";
-import path from "path";
-import connectDB from "./src/config/db.js";
-import authRoutes from "./src/routes/authRoutes.js";
-import productRoutes from "./src/routes/productRoutes.js";
-import uploadRoutes from "./src/routes/uploadRoutes.js";
-import feedbackRoutes from "./src/routes/feedbackRoutes.js";
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import path from 'path'
+import connectDB from './src/config/db.js'
+import authRoutes from './src/routes/authRoutes.js'
+import productRoutes from './src/routes/productRoutes.js'
+import uploadRoutes from './src/routes/uploadRoutes.js'
+import feedbackRoutes from './src/routes/feedbackRoutes.js'
 
-dotenv.config();
+dotenv.config()
+console.log('CLOUDINARY_CLOUD_NAME:', process.env.CLOUDINARY_CLOUD_NAME)
+console.log('CLOUDINARY_API_KEY:', process.env.CLOUDINARY_API_KEY)
 
 // ✅ Подключение к MongoDB
-connectDB();
+connectDB()
 
-const app = express();
+const app = express()
 
-app.use(cors()); // для локальной разработки
+app.use(cors()) // для локальной разработки
 
 // app.use(
 //   cors({
@@ -26,38 +28,38 @@ app.use(cors()); // для локальной разработки
 //   })
 // ); // PRod
 
-app.use(express.json());
+app.use(express.json())
 
 // ✅ Тестовый маршрут
-app.get("/", (req, res) => res.send("API is running"));
+app.get('/', (req, res) => res.send('API is running'))
 
 // ✅ Роуты пользователей
-app.use("/api/users", authRoutes);
+app.use('/api/users', authRoutes)
 
 // ✅ Роуты продуктов
-app.use("/api/products", productRoutes);
+app.use('/api/products', productRoutes)
 
 // ✅ Роут загрузки файлов
-app.use("/api/upload", uploadRoutes);
+app.use('/api/upload', uploadRoutes)
 
 // ✅ Роут фидбэка
-app.use("/api/feedback", feedbackRoutes);
+app.use('/api/feedback', feedbackRoutes)
 
 // ✅ Раздача папки uploads как статики
-const __dirname = path.resolve();
-app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 // ✅ Обработка ошибок
 app.use((err, req, res, next) => {
-  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-  res.status(statusCode);
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode
+  res.status(statusCode)
   res.json({
     message: err.message,
-    stack: process.env.NODE_ENV === "production" ? null : err.stack,
-  });
-});
+    stack: process.env.NODE_ENV === 'production' ? null : err.stack
+  })
+})
 
-const PORT = process.env.PORT || 5002;
+const PORT = process.env.PORT || 5002
 app.listen(PORT, () =>
   console.log(`✅ Server running on http://localhost:${PORT}`)
-);
+)
