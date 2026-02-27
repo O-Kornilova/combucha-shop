addd9e20db1afda0da6c844c1fce07326c62f571
O-Kornilova <aleksandra.kopachovets.ne@gmail.com>
2026-02-27 10:33:46 +0200

    fix routes of image

--- backend/src/routes/uploadRoutes.js
+++ backend/src/routes/uploadRoutes.js
@@ -1,43 +1,29 @@
-import express from "express";
-import multer from "multer";
-import path from "path";
+import express from 'express'
+import multer from 'multer'
+import { v2 as cloudinary } from 'cloudinary'
+import { CloudinaryStorage } from 'multer-storage-cloudinary'
 
-const router = express.Router();
+const router = express.Router()
 
-const storage = multer.diskStorage({
-  destination(req, file, cb) {
-    cb(null, "uploads/");
-  },
-  filename(req, file, cb) {
-    cb(
-      null,
-      `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
-    );
-  },
-});
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+})
 
-function checkFileType(file, cb) {
-  const filetypes = /jpg|jpeg|png/;
-  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-  const mimetype = filetypes.test(file.mimetype);
-
-  if (extname && mimetype) {
-    return cb(null, true);
-  } else {
-    cb("Images only!");
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: 'kombucha-shop',
+    allowed_formats: ['jpg', 'jpeg', 'png']
   }
-}
+})
 
-const upload = multer({
-  storage,
-  fileFilter: function (req, file, cb) {
-    checkFileType(file, cb);
-  },
-});
+const upload = multer({ storage })
 
-router.post("/multiple", upload.array("images", 10), (req, res) => {
-  const files = req.files.map((file) => `/${file.path}`);
-  res.send(files);
-});
+router.post('/multiple', upload.array('images', 10), (req, res) => {
+  const files = req.files.map(file => file.path) // це вже повний https:// URL
+  res.send(files)
+})
 
-export default router;
+export default router
